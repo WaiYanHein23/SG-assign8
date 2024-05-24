5061cc230198e6ea30e82e31361538ad1a710d3c
WaiYanHein23 <hwaiyan913@gmail.com>
2024-05-23 23:00:44 -0700

    main

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ let btn=document.querySelector(".btn");
 let text=document.querySelector("input");
 let paragraph=document.querySelector("p");
 
+let num=Math.floor(Math.random()*10 + 1);
+btn.addEventListener("click",()=>{
+if(text.value > num){
+    paragraph.innerHTML="Greater than Randon number";
+}else if(text.value<num){
+    paragraph.innerHTML="Less than Random number";
+}else{
+    paragraph.innerHTML="Correct";
+}
+}  
 
-btn.addEventListener("click",()=>
-    paragraph.innerHTML=(text.value+ (Math.random().toString(36))).slice(3).toUpperCase().charAt(3));
+);
